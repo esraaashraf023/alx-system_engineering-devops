414e297eb8fa7471a9fa52df0b8ba24be3369442
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-22 12:50:26 -0800

    updata

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Using Puppet, install flask from pip3
-
-package { 'Flask':
+# Install Flask with a specific version using pip3
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
