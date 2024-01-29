21402a6e68ea559f9961f6c999c26d4ee8f33427
esraaashraf023 <esraaashraf780567@gmail.com>
2024-01-29 20:16:59 +0200

    add file0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID"""
+
+import requests
+import sys
+import json
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 todo.py EMPLOYEE_ID')
+        sys.exit(1)
+    employee_id = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [task.get('title') for task in todos if task.get('completed')]
+    print("Employee {:s} is done with tasks({:d}/{:d}):".format(
+        user.get('name'), len(completed), len(todos)
+    ))
+
+    [print("\t {:s}".format(task)) for task in completed]
