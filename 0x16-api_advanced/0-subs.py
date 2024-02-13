6d75b72bc366c0e7109783a97db1bbb515b2e3a6
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-13 21:43:54 +0200

    Task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""queries the Reddit API..."""
+"""Module that queries the Reddit API"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    """
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    """prints the number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
-    # Construct the API URL for the subreddit
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Unfortunately I had to use a custom one'}
 
-        # Make the API request
-        response = requests.get(url, headers=headers)
-        data = response.json()
+    req = requests.get(url, headers=headers).json()
+    subs = req.get('data', {}).get('subscribers', 0)
 
-        # Extract the subscriber count
-        subscribers = data['data']['subscribers']
-        return subscribers
-    except (KeyError, ValueError):
-        # Invalid subreddit or error in API response
-        return 0
+    return subs
