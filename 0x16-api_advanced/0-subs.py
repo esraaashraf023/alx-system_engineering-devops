d33026773600343bc5be05770fffbdba931f2553
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-13 21:49:44 +0200

    Task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ import requests
 def number_of_subscribers(subreddit):
     """prints the number of subscribers for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
     headers = {'User-Agent': 'Unfortunately I had to use a custom one'}
 
     req = requests.get(url, headers=headers).json()
