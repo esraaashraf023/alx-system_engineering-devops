195fb278ae79c87ce72338af8eb13958794cecb8
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-20 19:01:51 +0200

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # Fix the issue based on the strace output
 
-exec { 'fix_apache_error':
+exec { 'fix_apache_issue':
   command     => '/usr/sbin/service apache2 restart',
   path        => '/usr/bin',
   refreshonly => true,
+  subscribe   => File['/path/to/modified_configuration_or_file'],
 }
-
