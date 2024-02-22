3804afc4d23a7e9a99a28217200e8f2376a67c37
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-22 22:01:22 +0200

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Increases the amount of traffic an Nginx server can handle.
 
-exec { 'set ulimit -n 4096':
-	command => "sed -i 's/15/4096/' /etc/default/nginx",
-		path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-}
+exec { 'set ulimit -n 2048':
+  command => "sed -i 's/15/2048/' /etc/default/nginx",
+  path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+  }
 
 exec { 'restart service':
-	command => 'service nginx restart',
-		path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
-}
+  command => 'service nginx restart',
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+  }
